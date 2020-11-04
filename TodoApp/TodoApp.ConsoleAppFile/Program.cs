07a1cc9992bd6ba0ecb7161be07404924c90df22
kwangbo <giggle_@naver.com>
2020-11-04 11:13:28 +0900

    스트림과 파일입축력 맛보기

---
+++ TodoApp/TodoApp.ConsoleAppFile/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using TodoApp.Models;
+
+namespace TodoApp.ConsoleAppFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            ITodoRepository _repository = new TodoRepositoryFile(@"D:\temp\Todos.txt");
+            List<Todo> todos = new List<Todo>();
+            todos = _repository.GetAll();
+
+            // 1. 기본 데이터 출력
+            foreach (var t in todos)
+            {
+                Console.WriteLine($"{t.Id}  - { t.Title} ({t.IsDone})");
+            }
+            Console.WriteLine();
+
+            //2. 데이터 입력 
+            Todo todo = new Todo { Title = "Database", IsDone = true };
+            _repository.Add(todo);
+
+            todos = _repository.GetAll(); //다시 로드
+
+            //3. 변경 데이터 출력
+            foreach (var t in todos)
+            {
+                Console.WriteLine($"{t.Id}  - {t.Title}({t.IsDone})");
+            }
+        }
+
+    }
+}
---
+++ TodoApp/TodoApp.Models/03_02_TodoRepositoryFile.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace TodoApp.Models
+{
+    public class TodoRepositoryFile : ITodoRepository
+    {
+
+        private readonly string _filePath = "";
+
+        private static List<Todo> _todos = new List<Todo>();
+        public TodoRepositoryFile()
+        {
+            _todos = new List<Todo>
+            {
+                new Todo { Id = 1, Title = "ASP.NET Core 학습", IsDone = false },
+                new Todo { Id = 2, Title = "Blazor 학습", IsDone = false },
+                new Todo { Id = 3, Title = "C# 학습", IsDone = true }
+            };
+        }
+
+
+        public TodoRepositoryFile(string filePath = @"D:\temp\Todos.txt")
+        {
+            this._filePath = filePath;
+            string[] todos = File.ReadAllLines(filePath, Encoding.Default);
+
+            foreach (var t in todos)
+            {
+
+                string[] line = t.Split(',');
+                Todo txtItem = new Todo();
+                txtItem.Id = Convert.ToInt32(line[0]);
+                txtItem.Title = line[1].ToString();
+                txtItem.IsDone = Convert.ToBoolean(line[2]);                
+                _todos.Add(txtItem);
+
+                //_todos.Add(new Todo {IsDone = Convert.ToBoolean(todos[2]), Title = todos[1], Id = Convert.ToInt32(todos[0]) });
+
+
+            }
+
+
+        }
+
+        public void Add(Todo model)
+        {
+            model.Id = _todos.Max(t => t.Id) + 1;
+            _todos.Add(model);
+
+            //파일저장 
+            string data = "";
+            foreach (var t in _todos)
+            {
+                data += $"{t.Id},{t.Title},{t.IsDone}{Environment.NewLine}"  ;
+            }
+
+            using (StreamWriter sw =new StreamWriter(_filePath))
+            {
+                sw.Write(data);
+                sw.Close();
+                //sw.Dispose();
+            }
+
+        }
+        public List<Todo> GetAll()
+        {
+            return _todos.ToList();
+        }
+    }
+
+}
+
