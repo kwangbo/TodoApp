f5d2c064bfdfe30d6b91011c90a707e84e4d09ed
kwangbo <giggle_@naver.com>
2020-11-04 17:30:37 +0900

    네트워크 프로그래밍 맛보기 데모

---
+++ TodoApp/TodoApp.WebApIs/Controllers/TodosController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TodoApp.Models;
+
+namespace TodoApp.WebApIs.Controllers
+{
+    [Route("api/[Controller]")]
+    public class TodosController : ControllerBase
+    {
+        private readonly ITodoRepository _repository;
+        public TodosController()
+        {
+            _repository = new TodoRepositoryJson(@"D:\temp\Todos.json");
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            // return Content("안녕하세요");
+
+            return Ok(_repository.GetAll());
+            
+
+        }
+
+
+        [HttpPost]
+        public IActionResult Add([FromBody] Todo dto)
+        {
+            _repository.Add(dto);
+            return Ok(dto);
+        }
+
+    }
+}
